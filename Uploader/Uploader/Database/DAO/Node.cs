3b45a2ff9dd46b16484c3e655fd9def8baeba24d
Jan.Hyka <hykajan@hotmail.com>
2016-09-28 11:08:47 +0200

    Initial database model

---
+++ Uploader/Uploader/Database/DAO/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.DAO
+{
+    public class Node
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(128)]
+        public string Label { get; set; }
+        public virtual List<AdjacentNode> AdjacentNodes { get; set; }
+    }
+}
---
+++ Uploader/Uploader/Database/GraphContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database.DAO;
+
+namespace Database
+{
+    public class GraphContext : DbContext
+    {
+        public DbSet<Node> Nodes { get; set; }
+        public DbSet<AdjacentNode> AdjacentNodes { get; set; }
+    }
+}
